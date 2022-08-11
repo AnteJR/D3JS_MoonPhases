5d6002c18794832674e96cb69a2facb724d206d0
Joël Rimaz <jo.ante2011@gmail.com>
2022-08-11 13:14:14 +0200

    Lead to the lunar calendar per month operationnal
    Needs a "go back" button though

--- JS/main.js
+++ JS/main.js
@@ -45,27 +45,74 @@ let monCSV = d3.csv("Data/Moonphases.csv", function (d) {
         .attr("class", "txtCalendarYear")
         .attr("x", (d, i) => {                                      // if I want to put them at slightly random places, remove commented Math.ceil(...)
             if (i < 6) return (largeur / 15) + (largeur / 6) * (i) // + Math.ceil((Math.random() - 0.5) * 100)
-            else if (i == 6 || i == 7 || i == 10 || i == 11) return (largeur / 15) + (largeur / 6) * (i - 6) // + Math.ceil((Math.random() - 0.5) * 100)
-            else if (i == 8 || i == 9 || i == 12 || i == 13) return (largeur / 15) + (largeur / 6) * (i - 8) // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i == 6 || i == 7) return (largeur / 15) + (largeur / 6) * (i - 6) // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i == 8 || i == 9) return (largeur / 15) + (largeur / 6) * (i - 4) // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i == 10 || i == 11) return (largeur / 15) + (largeur / 6) * (i - 10) // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i == 12 || i == 13) return (largeur / 15) + (largeur / 6) * (i - 8) // + Math.ceil((Math.random() - 0.5) * 100)
             else if (i > 13 && i < 20) return (largeur / 15) + (largeur / 6) * (i - 14) // + Math.ceil((Math.random() - 0.5) * 100)
             else if (i >= 20) return (largeur / 15) + (largeur / 6) * (i - 20) // + Math.ceil((Math.random() - 0.5) * 100)
         })
         .attr("y", (d, i) => {
             if (i < 6) return hauteur / 10 // + Math.ceil((Math.random() - 0.5) * 100)
-            else if (i == 6 || i == 7 || i == 10 || i == 11) return hauteur / 10 * 3 // + Math.ceil((Math.random() - 0.5) * 100)
-            else if (i == 8 || i == 9 || i == 12 || i == 13) return hauteur / 10 * 5 // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i == 6 || i == 7 || i == 8 || i == 9) return hauteur / 10 * 3 // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i == 10 || i == 11 || i == 12 || i == 13) return hauteur / 10 * 5 // + Math.ceil((Math.random() - 0.5) * 100)
             else if (i > 13 && i < 20) return hauteur / 10 * 7 // + Math.ceil((Math.random() - 0.5) * 100)
             else if (i >= 20) return hauteur / 10 * 9 // + Math.ceil((Math.random() - 0.5) * 100)
         })
         .style("font-style", "italic")
         .style("font-size", "2em")
+        .style("cursor", "pointer")
         .style("opacity", "0")
-});
+        .style("display", "none")
+        .on("click", (e) => {
+            d3.selectAll(".txtCalendarYear").transition().delay((d, i) => 50 * i).duration(500).style("opacity", "0");
+            setTimeout(() => {
+                d3.selectAll(".txtCalendarYear").style("display", "none");
+                d3.selectAll(".txtCalendarMonth").transition().delay((d, i) => 100 * i).duration(1000).style("display", "block").style("opacity", "0.7");
+                yearSelected.transition().duration(1000).style("display", "block").style("opacity", "1").text(e.path[0].__data__[0]);
+            }, 1500);
+        });
 
-// wait unil CSV is imported into the dataTable var, and then act
-setTimeout(() => {
-    // idk if it'll be useful but i'll keep it for now
-}, 250);
+    let monthTable = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+
+    svgSpace.append("g")
+        .attr("id", "monthTxt")
+        .selectAll("text")
+        .data(monthTable)
+        .enter()
+        .append("text")
+        .text((d) => d)
+        .attr("class", "txtCalendarMonth")
+        .attr("x", (d, i) => {
+            if (i == 0 || i == 1) return (largeur / 15) + (largeur / 6) * (i);
+            else if (i == 2 || i == 3) return (largeur / 15) + (largeur / 6) * (i + 2);
+            else if (i == 4 || i == 5) return (largeur / 15) + (largeur / 6) * (i - 4);
+            else if (i == 6 || i == 7) return (largeur / 15) + (largeur / 6) * (i - 2);
+            else if (i > 7) return (largeur / 15) + (largeur / 6) * (i - 7);
+        })
+        .attr("y", (d, i) => {
+            if (i == 0 || i == 1 || i == 2 || i == 3) return hauteur / 10 * 3;
+            else if (i == 4 || i == 5 || i == 6 || i == 7) return hauteur / 10 * 5;
+            else if (i > 7) return hauteur / 10 * 7;
+        })
+        .style("font-style", "italic")
+        .style("font-size", "2em")
+        .style("cursor", "pointer")
+        .style("opacity", "0")
+        .style("display", "none")
+        .on("click", (e) => {
+            myMoon.transition().duration(1000).attr("transform", "translate(" + largeur / 2 + "," + 0 + ") scale(-1,1)");
+            titleApp.transition().duration(500).style("opacity","0");
+
+            d3.selectAll(".txtCalendarMonth").transition().delay((d, i) => 50 * i).duration(500).style("opacity", "0");
+            setTimeout(() => {
+                d3.selectAll(".txtCalendarMonth").style("display", "none");
+            }, 2400);
+
+            let yearTxt = yearSelected.text();
+            yearSelected.transition().duration(1000).style("opacity", "0").transition().duration(100).attr("y", hauteur / 10).transition().duration(1000).style("opacity", "1").text(e.path[0].__data__ + " " + yearTxt);
+        });
+});
 
 /* MAIN CODE WILL GO THERE */
 
@@ -84,6 +131,7 @@ let titleApp = svgSpace
     .attr("x", largeur / 2)
     .attr("y", hauteur / 2)
     .style("font-size", "3em")
+    .style("cursor", "default");
 
 let subTitleApp = svgSpace
     .append("text")
@@ -91,6 +139,7 @@ let subTitleApp = svgSpace
     .attr("fill", "white")
     .attr("x", largeur / 2)
     .attr("y", hauteur / 2 + 25)
+    .style("cursor", "default");
 
 let startUpButton = svgSpace
     .append("text")
@@ -101,14 +150,27 @@ let startUpButton = svgSpace
     .style("text-anchor", "middle")
     .style("font-size", "4em")
     .style("opacity", "1")
+    .style("cursor", "pointer")
     .on("click", () => {
         startUpButton.transition().duration(200).style("opacity", "0");
         setTimeout(() => { startUpButton.remove() }, 200);
         myMoon.transition().duration(1000).attr("transform", "translate(" + largeur / 2 + "," + ((hauteur / 2) - (hauteur / 10)) + ") scale(-1,1)");
         titleApp.transition().duration(1000).attr("y", (hauteur / 2) - (hauteur / 10));
         subTitleApp.transition().duration(1000).attr("y", (hauteur / 2) - (hauteur / 10) + 25).style("opacity", "0");
-        d3.selectAll(".txtCalendarYear").transition().delay((d, i) => 100 * i).duration(3000).style("opacity", "0.7");
-    })
+        d3.selectAll(".txtCalendarYear").transition().delay((d, i) => 100 * i).duration(1000).style("display", "block").style("opacity", "0.7");
+    });
+
+let yearSelected = svgSpace
+    .append("text")
+    .text("")
+    .attr("fill", "white")
+    .attr("x", largeur / 2)
+    .attr("y", hauteur / 10 * 8)
+    .style("cursor", "pointer")
+    .style("text-anchor", "middle")
+    .style("font-size", "5em")
+    .style("opacity", "0")
+    .style("display", "none");
 
 d3.timer(changeMoon);
 
