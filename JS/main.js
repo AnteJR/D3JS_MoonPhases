305952dc2dd868fab153bb0eea43539c10ad8261
Joël Rimaz <jo.ante2011@gmail.com>
2022-08-11 11:45:47 +0200

    Startup and date placement OK

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,17 @@
+// Some useful const
 const largeur = window.innerWidth - 20;
 const hauteur = window.innerHeight - 20;
 const rayon = (window.innerHeight - 20) / 4;
 const pi = Math.PI;
 
 let dataTable;
+
+// SVG canvas setup
+let svgSpace = d3
+    .select("body")
+    .append("svg")
+    .attr("width", largeur)
+    .attr("height", hauteur);
 
 // importing CSV
 let monCSV = d3.csv("Data/Moonphases.csv", function (d) {
@@ -15,8 +23,43 @@ let monCSV = d3.csv("Data/Moonphases.csv", function (d) {
         percentLight: Math.PI * +d.percentage,
         darksDirection: d.darkFaces
     }
-}).then((k) => {
-    dataTable = k
+}).then((dataMoon) => {
+    dataTable = dataMoon;
+
+    let yearTable = [];
+    let previousYear = "";
+    dataMoon.forEach((e) => {
+        if (previousYear != e.year) {
+            previousYear = e.year;
+            yearTable.push([e.year]);
+        }
+    });
+
+    svgSpace.append("g")
+        .attr("id", "yearTxt")
+        .selectAll("text")
+        .data(yearTable)
+        .enter()
+        .append("text")
+        .text((d) => d)
+        .attr("class", "txtCalendarYear")
+        .attr("x", (d, i) => {                                      // if I want to put them at slightly random places, remove commented Math.ceil(...)
+            if (i < 6) return (largeur / 15) + (largeur / 6) * (i) // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i == 6 || i == 7 || i == 10 || i == 11) return (largeur / 15) + (largeur / 6) * (i - 6) // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i == 8 || i == 9 || i == 12 || i == 13) return (largeur / 15) + (largeur / 6) * (i - 8) // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i > 13 && i < 20) return (largeur / 15) + (largeur / 6) * (i - 14) // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i >= 20) return (largeur / 15) + (largeur / 6) * (i - 20) // + Math.ceil((Math.random() - 0.5) * 100)
+        })
+        .attr("y", (d, i) => {
+            if (i < 6) return hauteur / 10 // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i == 6 || i == 7 || i == 10 || i == 11) return hauteur / 10 * 3 // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i == 8 || i == 9 || i == 12 || i == 13) return hauteur / 10 * 5 // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i > 13 && i < 20) return hauteur / 10 * 7 // + Math.ceil((Math.random() - 0.5) * 100)
+            else if (i >= 20) return hauteur / 10 * 9 // + Math.ceil((Math.random() - 0.5) * 100)
+        })
+        .style("font-style", "italic")
+        .style("font-size", "2em")
+        .style("opacity", "0")
 });
 
 // wait unil CSV is imported into the dataTable var, and then act
@@ -29,13 +72,6 @@ setTimeout(() => {
 
 
 /* TITLE */
-// SVG canvas setup
-let svgSpace = d3
-    .select("body")
-    .append("svg")
-    .attr("width", largeur)
-    .attr("height", hauteur);
-
 let myMoon = svgSpace
     .append("path")
     .attr("fill", "white")
@@ -55,6 +91,24 @@ let subTitleApp = svgSpace
     .attr("fill", "white")
     .attr("x", largeur / 2)
     .attr("y", hauteur / 2 + 25)
+
+let startUpButton = svgSpace
+    .append("text")
+    .text("start.")
+    .attr("fill", "white")
+    .attr("x", largeur / 2)
+    .attr("y", hauteur / 10 * 9)
+    .style("text-anchor", "middle")
+    .style("font-size", "4em")
+    .style("opacity", "1")
+    .on("click", () => {
+        startUpButton.transition().duration(200).style("opacity", "0");
+        setTimeout(() => { startUpButton.remove() }, 200);
+        myMoon.transition().duration(1000).attr("transform", "translate(" + largeur / 2 + "," + ((hauteur / 2) - (hauteur / 10)) + ") scale(-1,1)");
+        titleApp.transition().duration(1000).attr("y", (hauteur / 2) - (hauteur / 10));
+        subTitleApp.transition().duration(1000).attr("y", (hauteur / 2) - (hauteur / 10) + 25).style("opacity", "0");
+        d3.selectAll(".txtCalendarYear").transition().delay((d, i) => 100 * i).duration(3000).style("opacity", "0.7");
+    })
 
 d3.timer(changeMoon);
 
@@ -81,6 +135,6 @@ function moon(m) {
     let flip2;
     if (m < pi / 2 || (pi <= m && m < 3 * pi / 2)) flip2 = 0;
     else flip2 = 1;
-    
+
     return "M" + [0, rayon] + " A" + [rotation1, rayon, 0, 0, flip1, 0, -rayon] + " A" + [rotation2, rayon, 0, 0, flip2, 0, rayon];
 }
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# D3_MoonPhases_Art
+# D3. Moon Phases
 
- A D3.js artful project that displays the moon phases from my birth year (1997) to 2022.
+ A D3.js artful data-driven project that displays the moon phases from my birth year of 1997 to the present day of 2022.
 
 ## Data and its collection
 
 [Time and Date](https://www.timeanddate.com/moon/phases/?year=20229) was used to get the data from the moon phases. I had to enter manually all the 4 phases of the moon, and the year, month and day they occured.
 
-I also took a note of the percentage of lighting the moon is recieving, and what direction the shadow faces.
+I also took a note of the percentage of lighting the moon is recieving, which I converted to a multiple of pi (from 0 to 2), and what direction the shadow faces.
